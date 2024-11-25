35792f771ca3cb79787a08f723da105bc9b859b1
JaisyMiqdam <a710220022@student.ums.ac.id>
2024-11-25 17:16:11 +0700

    chore: base api

--- src/routes/routing.js
+++ src/routes/routing.js
@@ -1,1 +1,32 @@
-// here is the routing
+const books = [];
+  
+  const routes = [
+    {
+      method: 'GET',
+      path: '/',
+      handler: (request, h) => {
+        return h.response({ message: 'Success' }).code(200);
+      },
+    },
+    {
+      method: 'GET',
+      path: '/books',
+      handler: (request, h) => {
+        return books;
+      },
+    },
+    {
+      method: 'GET',
+      path: '/books/{id}',
+      handler: (request, h) => {
+        const book = books.find(b => b.id === parseInt(request.params.id, 10));
+        if (!book) {
+          return h.response({ message: 'Book not found' }).code(404);
+        }
+        return book;
+      },
+    },
+  ];
+  
+  module.exports = routes;
+  
